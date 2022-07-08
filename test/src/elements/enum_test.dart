3dee66ffabea37020131b56ba5afd591cbcca59c
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2022-07-08 07:43:53 +0200

    test: add another test for Enum and EnumValue

---
+++ test/src/elements/enum_test.dart
@@ -0,0 +1,34 @@
+import 'package:coder/coder.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group(
+    'Enum',
+    () {
+      test(
+        'supports property equality',
+        () {
+          expect(
+            const Enum(name: 'Status'),
+            equals(const Enum(name: 'Status')),
+          );
+        },
+      );
+    },
+  );
+
+  group(
+    'EnumValue',
+    () {
+      test(
+        'supports property equality',
+        () {
+          expect(
+            const EnumValue(name: 'todo'),
+            equals(const EnumValue(name: 'todo')),
+          );
+        },
+      );
+    },
+  );
+}
