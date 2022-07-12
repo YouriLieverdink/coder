bc2bdf7e878971cac4266502a3ab6fb739d2ddf3
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2022-07-12 21:09:26 +0200

    feat: add many, many more builder options

--- test/src/builder_test.dart
+++ test/src/builder_test.dart
@@ -11,15 +11,230 @@ void main() {
     'Builder',
     () {
       test(
-        'should emit a equal to statement',
+        'should emit an add operation',
+        () {
+          const left = Literal(42);
+          const right = Literal(8);
+
+          final operation = left.add(right);
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                42 + 8
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an and operation',
         () {
           const left = Literal('cat');
           const right = Literal('dog');
 
-          final statement = left.equalTo(right);
+          final operation = left.and(right);
 
           Expect(
-            statement,
+            operation,
+            const Equals(
+              '''
+                'cat' && 'dog'
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an as operation',
+        () {
+          const left = Literal('cat');
+          const right = Literal('dog');
+
+          final operation = left.as(right);
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                'cat' as 'dog'
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an assign operation',
+        () {
+          const left = Literal('cat');
+          const right = Literal('dog');
+
+          final operation = left.assign(right);
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                'cat' = 'dog'
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an assign coalescing operation',
+        () {
+          const left = Literal('cat');
+          const right = Literal('dog');
+
+          final operation = left.assignCoalescing(right);
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                'cat' ??= 'dog'
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an assign const operation',
+        () {
+          const left = Literal('cat');
+
+          final operation = left.assignConst('myCat');
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                const myCat = 'cat'
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an assign final operation',
+        () {
+          const left = Literal('cat');
+
+          final operation = left.assignFinal('myCat');
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                final myCat = 'cat'
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an assign var operation',
+        () {
+          const left = Literal('cat');
+
+          final operation = left.assignVar('myCat');
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                var myCat = 'cat'
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an awaited operation',
+        () {
+          const left = Static('cat');
+
+          final operation = left.await_();
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                await cat
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit a call operation',
+        () {
+          const left = Static('Cat');
+
+          final operation = left.call();
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                Cat()
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an divide operation',
+        () {
+          const left = Literal(42);
+          const right = Literal(8);
+
+          final operation = left.divide(right);
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                42 / 8
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit a equal to operation',
+        () {
+          const left = Literal('cat');
+          const right = Literal('dog');
+
+          final operation = left.equalTo(right);
+
+          Expect(
+            operation,
             const Equals(
               '''
                 'cat' == 'dog'
@@ -31,18 +246,409 @@ void main() {
       );
 
       test(
-        'should emit a not equal to statement',
+        'should emit a greater than operation',
         () {
           const left = Literal('cat');
           const right = Literal('dog');
 
-          final statement = left.notEqualTo(right);
+          final operation = left.greaterThan(right);
 
           Expect(
-            statement,
+            operation,
+            const Equals(
+              '''
+                'cat' > 'dog'
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit a reversed greater than operation',
+        () {
+          const left = Literal('cat');
+          const right = Literal('dog');
+
+          final operation = left.greaterThan(right, reverse: true);
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                'cat' < 'dog'
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit a greater than or equal operation',
+        () {
+          const left = Literal('cat');
+          const right = Literal('dog');
+
+          final operation = left.greaterThanOrEqualTo(right);
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                'cat' >= 'dog'
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit a reversed greater than or equal operation',
+        () {
+          const left = Literal('cat');
+          const right = Literal('dog');
+
+          final operation = left.greaterThanOrEqualTo(right, reverse: true);
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                'cat' <= 'dog'
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an index operation',
+        () {
+          const left = Static('cats');
+          const right = Literal(0);
+
+          final operation = left.index(right);
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                cats[0]
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an is operation',
+        () {
+          const left = Literal('cat');
+          const right = Literal('dog');
+
+          final operation = left.is_(right);
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                'cat' is 'dog'
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an is not operation',
+        () {
+          const left = Literal('cat');
+          const right = Literal('dog');
+
+          final operation = left.isNot(right);
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                'cat' is! 'dog'
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an modulo operation',
+        () {
+          const left = Literal(42);
+          const right = Literal(8);
+
+          final operation = left.modulo(right);
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                42 % 8
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an multiply operation',
+        () {
+          const left = Literal(42);
+          const right = Literal(8);
+
+          final operation = left.multiply(right);
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                42 * 8
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit a negate operation',
+        () {
+          const left = Literal(42);
+
+          final operation = left.negate();
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                !42
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit a not equal to operation',
+        () {
+          const left = Literal('cat');
+          const right = Literal('dog');
+
+          final operation = left.notEqualTo(right);
+
+          Expect(
+            operation,
             const Equals(
               '''
                 'cat' != 'dog'
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit a not equal to operation',
+        () {
+          const left = Literal('cat');
+          const right = Literal('dog');
+
+          final operation = left.or(right);
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                'cat' || 'dog'
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit a property operation',
+        () {
+          const left = Static('cat');
+
+          final operation = left.property('name');
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                cat.name
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit a property asserted',
+        () {
+          const left = Static('cat');
+
+          final operation = left.propertyAssert('name');
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                cat!.name
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit a property cascaded',
+        () {
+          const left = Static('cat');
+
+          final operation = left.propertyCascade('name');
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                cat..name
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit a property conditional operation',
+        () {
+          const left = Static('cat');
+
+          final operation = left.propetyConditional('name');
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                cat?.name
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an returned operation',
+        () {
+          const left = Static('cat');
+
+          final operation = left.return_();
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                return cat
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an spread operation',
+        () {
+          const left = Static('cat');
+
+          final operation = left.spread();
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                ...cat
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an subtract operation',
+        () {
+          const left = Literal(42);
+          const right = Literal(8);
+
+          final operation = left.subtract(right);
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                42 - 8
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an thrown operation',
+        () {
+          const left = Static('cat');
+
+          final operation = left.throw_();
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                throw cat
+              ''',
+              emitter: emitter,
+            ),
+          );
+        },
+      );
+
+      test(
+        'should emit an yielded operation',
+        () {
+          const left = Static('cat');
+
+          final operation = left.yield_();
+
+          Expect(
+            operation,
+            const Equals(
+              '''
+                yield cat
               ''',
               emitter: emitter,
             ),
