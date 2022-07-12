068a026489c895661dfa21d462d578069ed0411c
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2022-07-12 10:23:18 +0200

    docs: add explanation of builder mixin

--- lib/src/builder.dart
+++ lib/src/builder.dart
@@ -3,7 +3,22 @@ library builder;
 import 'package:orchestrator/orchestrator.dart';
 
 /// {@template builder}
+/// Used to specify whether an [Element] can be used in other to create complex
+/// builder-like statements.
 ///
+/// This mixin is especially useful for the generation of complex Dart code
+/// with multiple elements instead of using a single [Static] element with
+/// template literals.
+///
+/// ```dart
+/// const element = Static("'cat' == 'dog'");
+/// ```
+///
+/// Can also be written as:
+///
+/// ```dart
+/// const element = Literal('cat').equalTo(Literal('dog'));
+/// ```
 /// {@endtemplate}
 mixin Builder on Element {
   /// Returns the equal to operation of `this` and [other].
