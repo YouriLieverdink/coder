96991579ecefecc16084b9c5ab73443d2ec12a9c
Youri <89806453+YouriLieverdink@users.noreply.github.com>
2022-08-12 09:52:55 +0200

    feat: add ability to generate a library (#66)

---
+++ lib/src/elements/library.dart
@@ -0,0 +1,22 @@
+part of element;
+
+/// {@template library}
+/// Configuration for defining a library.
+/// {@endtemplate}
+class Library extends Element {
+  /// {@macro library}
+  const Library({
+    this.name,
+    this.directives = const [],
+    this.elements = const [],
+  });
+
+  /// The name of this library.
+  final String? name;
+
+  /// The directives which this library should use.
+  final List<Directive> directives;
+
+  /// The elements contained within this library.
+  final List<Element> elements;
+}
